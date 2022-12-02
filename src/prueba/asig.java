05569eea9cc04e68bb2357a54fe5c805239ba064
fatoq <79267359+fatoq@users.noreply.github.com>
2022-12-01 21:25:57 -0500

    se crearon metodos en asig

--- src/prueba/asig.java
+++ src/prueba/asig.java
@@ -9,5 +9,23 @@ package prueba;
  * @author david
  */
 public class asig {
-    
+        private int[] parciales = new int[3];
+	private String nombre;
+	/*
+	public Asignatura(String nombre) {
+		this.nombre = nombre;
+	}
+*/
+	
+	public String getNombre() {
+		return nombre;
+	}
+     public void registrarNotaParcial(int nota, int posicion) {
+		parciales[posicion] = nota;
+	}
+
+	public int getNotaDefinitiva() {
+		int sumaNotas = parciales[0] + parciales[1] + parciales[2];
+		return sumaNotas / 3;
+	}
 }
