23eaaaad72c597d72ab016c625ec076c6ca6c83e
fatoq <79267359+fatoq@users.noreply.github.com>
2022-12-01 20:06:11 -0500

    mensaje

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -14,6 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //nuevo 
         // TODO code application logic here
     }
     
